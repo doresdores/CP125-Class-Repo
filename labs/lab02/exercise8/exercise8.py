902ba18f151e80a6ae28f6ba92f10f0bfcebc3ef
doresdores <dyrothdores@gmail.com>
2025-12-29 12:51:15 +0800

    lab

--- labs/lab02/exercise8/exercise8.py
+++ labs/lab02/exercise8/exercise8.py
@@ -3,7 +3,7 @@ def calculate_bounce_height(current_height):
     Calculate the next bounce height (80% of current).
     """
     # TODO: Implement this
-    pass
+    return current_height * 0.8
 
 
 def is_ball_stopped(height):
@@ -11,15 +11,26 @@ def is_ball_stopped(height):
     Check if the ball has stopped (height < 1).
     """
     # TODO: Implement this
-    pass
+    return height< 1
 
 
-def calculate_bounce_count(initial_height):
+def calculate_bounce_count(initial_height,):
     """
     Count how many times the ball bounces.
     """
     # TODO: Implement this
-    pass
+    count = 0
+    height = initial_height
+
+    if height == 0:
+        return 0
+
+    while True:
+        height = calculate_bounce_height(height)
+        count += 1
+        if is_ball_stopped(height):
+            break
+    return count
 
 
 def calculate_total_distance(initial_height):
@@ -27,4 +38,18 @@ def calculate_total_distance(initial_height):
     Calculate total distance traveled.
     """
     # TODO: Implement this
-    pass
+    if initial_height == 0:
+        return 0
+    
+    total_distance = initial_height  
+    height = initial_height
+
+    while True:
+        height = calculate_bounce_height(height)
+        total_distance += height  
+
+        if is_ball_stopped(height):
+            break
+        
+        total_distance += height  
+    return total_distance
--- labs/lab02/exercise9/exercise9.py
+++ labs/lab02/exercise9/exercise9.py
@@ -3,7 +3,7 @@ def calculate_xp_required(current_level):
     Calculate XP needed for next level (level * 100).
     """
     # TODO: Implement this
-    pass
+    return current_level *100
 
 
 def can_level_up(current_xp, required_xp):
@@ -11,20 +11,28 @@ def can_level_up(current_xp, required_xp):
     Check if player has enough XP to level up.
     """
     # TODO: Implement this
-    pass
-
+    return current_xp >= required_xp
+        
 
 def calculate_final_level(total_xp):
     """
     Calculate the final level reached.
     """
     # TODO: Implement this
-    pass
-
+    level = 1
+    while total_xp >= calculate_xp_required(level):
+       total_xp -= calculate_xp_required(level)
+       level += 1
+    return level
 
 def calculate_remaining_xp(total_xp):
     """
     Calculate XP leftover after leveling.
     """
     # TODO: Implement this
-    pass
+    level = 1
+    while total_xp >= calculate_xp_required(level):
+       total_xp -= calculate_xp_required(level)
+       level += 1
+    return total_xp
+
