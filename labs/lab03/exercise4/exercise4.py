1cbaabc1819b9ff5714386d2cfc6bca7796cb0f3
doresdores <dyrothdores@gmail.com>
2026-01-05 13:07:18 +0800

    update

--- labs/lab03/exercise4/exercise4.py
+++ labs/lab03/exercise4/exercise4.py
@@ -1,2 +1,8 @@
 def has_warming_trend(temps):
-    pass
+    if len(temps) < 3 :
+        return False
+    
+    for i in range(len(temps)-2):
+        if temps[i] < temps[i + 1] < temps[i + 2]:
+            return True
+    return False
