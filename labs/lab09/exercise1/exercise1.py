b1c7d53565c983ffdad9dbc2e17a0bfeff9293ed
doresdores <dyrothdores@gmail.com>
2026-04-02 10:07:16 +0800

    lab0

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-
 def explore_data(filename):
-    pass
+    df = pd.read_cvs(filename)
+    total_students = len(df)
+    subject = [Math, Science, English]
+    math_average = df["Math"].mean()
+    highest_math_student = df.loc[df["Math"],]
+    
+    return {
+        "total_students": total_students,
+        "subjects": subject,
+        "math_average": math_average,
+        "highest_math_student": highest_math_student
+    }
