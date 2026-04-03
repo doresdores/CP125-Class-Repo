6a6f1d99c1f525a6f19f9c90cb29fe87c78e5411
doresdores <dyrothdores@gmail.com>
2026-04-03 08:08:47 +0800

    lab09

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
-
 def compare_averages(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    AVG = {
+         "Math": round(df["Math"].mean(), 1),
+        "Science": round(df["Science"].mean(), 1),
+        "English": round(df["English"].mean(), 1)
+    }
+
+    best_subject = max(AVG, key=AVG.get)
+    worst_subject = min(AVG, key=AVG.get)
+
+    AVG["best_subject"] = best_subject
+    AVG["worst_subject"] = worst_subject
+
+    return AVG
+
+
--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -3,4 +3,18 @@ import matplotlib.pyplot as plt
 
 
 def show_math_trend(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    math_scores = df["Math"]
+    plt.plot(df.index, math_scores)
+
+    plt.xlabel("Student Index")
+    plt.ylabel("Math Score")
+    plt.title("Math Score Trends")
+    
+    plt.show()
+
+    
+    
+
+    
